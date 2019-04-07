2993000a094a9f4516726051c534a7090361447d
Chris Piazza <piazza.chris@gmail.com>
2019-04-07 19:41:35 -0400

    [react-12] adds initail users page and component

--- src/components/AppContent.jsx
+++ src/components/AppContent.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom'
 import Dashboard from './dashboard/Dashboard'
 import Players from './players/Players'
+import Users from './users/Users'
 
 class AppContent extends Component {
   render () {
@@ -9,6 +10,7 @@ class AppContent extends Component {
       <div id="app-content">
         <Route exact path="/" component={Dashboard} />
         <Route exact path="/players" component={Players} />
+        <Route exact path="/users" component={Users} />
       </div>
     )
   }
