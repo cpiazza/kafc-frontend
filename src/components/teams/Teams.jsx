b4d4bf6f8feea5d763aa9b541ad4786e35815e1e
Alex Piazza <alexpiazza@Alexs-MacBook-Pro-3.local>
2019-06-25 02:01:14 -0400

    [react-13] add teams page and component

---
+++ src/components/teams/Teams.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Teams extends Component {
+    render () {
+        return (
+           <div>
+               <h1>All The Chelsea's </h1>
+           </div>
+        )
+    }
+}
+
+export default Teams;
