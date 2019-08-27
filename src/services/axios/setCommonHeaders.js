e96acb69e9259a01a9a52578df2656c69bae61cb
Chris Piazza <piazza.chris@gmail.com>
2019-08-27 19:50:12 -0400

    [jwt] sets the authorization header in the request inteceptor

--- src/services/axios/setCommonHeaders.js
+++ src/services/axios/setCommonHeaders.js
@@ -1,11 +1,3 @@
-import store from '../../appStore'
-
 export function setCommonHeaders(Axios) {
-  const state = store.getState();
-
   Axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*'
-
-  if (state.auth.user !== undefined) {
-      Axios.defaults.headers.common['Authorization'] = state.auth.user.jwt
-  }
 }
